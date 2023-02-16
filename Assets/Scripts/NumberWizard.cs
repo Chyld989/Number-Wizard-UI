f7ce63dde62619f9a2cc0770b78992a0f60bf839
Chyld989 <chyld989@gmail.com>
2023-02-16 12:28:25 -0600

    Initial commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+	[SerializeField] int MaxGuess;
+	[SerializeField] int MinGuess;
+	[SerializeField] TextMeshProUGUI GuessText;
+	int Guess;
+
+	// Start is called before the first frame update
+	void Start() {
+		StartGame();
+	}
+
+	void StartGame() {
+		UpdateGuess();
+	}
+
+	public void OnPressHigher() {
+		if (MinGuess != MaxGuess) {
+			MinGuess = Guess + 1;
+			UpdateGuess();
+		} else {
+			Guess = MinGuess;
+			DisplayGuess();
+		}
+	}
+
+	public void OnPressLower() {
+		if (MinGuess != MaxGuess) {
+			MaxGuess = Guess - 1;
+			UpdateGuess();
+		} else {
+			Guess = MinGuess;
+			DisplayGuess();
+		}
+	}
+
+	void UpdateGuess() {
+		Guess = UnityEngine.Random.Range(MinGuess, MaxGuess + 1);
+		DisplayGuess();
+	}
+
+	void DisplayGuess() {
+		GuessText.text = Guess.ToString("N0");
+	}
+}
